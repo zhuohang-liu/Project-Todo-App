1fa66c4850c818652fc3e3c0aee0195b286d0ba9
Peidi Xie <cindyxie@usc.edu>
2023-07-03 23:28:03 -0700

    remove entries

--- components/Entry.js
+++ components/Entry.js
@@ -1,17 +1,41 @@
+import { useState, useContext } from 'react';
+import { useCollapse } from 'react-collapsed';
+import TodoContext from './context';
+
+function Delete({entryid, date}) {
+    const { removeTodos } = useContext(TodoContext);
+
+    return (
+        <button onClick={() => removeTodos(entryid, date)}>Delete</button>
+    );
+}
 
 export default function Entry({ entry, show_date, show_time }) {
+    
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+
     let entry_date = entry.date;
     let entry_time = entry.time;
     let entry_title = entry.title;
+
+    let entry_key = entry_date + "T" + entry_time + "_" + entry_title;
+    let _date = entry_date;
 
     if(entry_date === "-1" || !show_date) entry_date = "";
     if(entry_time === "-1" || !show_time) entry_time = "";
 
     return (
-        <div>
-            <div className="entry entry-date">{entry_date}</div>
-            <div className="entry entry-time">{entry_time}</div>
-            <div className="entry entry-title">{entry_title}</div>
+        <div className="collapsible">
+            <div className="header_entry" {...getToggleProps()}>
+                <div className="entry entry-title">{entry_title}</div>
+                <div className="entry entry-date">{entry_date}</div>
+                <div className="entry entry-time">{entry_time}</div>
+            </div>
+            <div {...getCollapseProps()}>
+                <div>
+                    <Delete entryid={entry_key} date={_date}/>
+                </div>
+            </div>
         </div>
     );
 }
